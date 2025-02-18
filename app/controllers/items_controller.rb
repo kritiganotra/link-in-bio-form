319096a618a9edeebacae15aa6180c47f9a4115f
kritiganotra <kritiganotra@gmail.com>
2025-02-18 20:53:45 +0000

    all done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,21 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def form
+    render({ :template => "item_templates/form" })
+  end
+
+  def add
+    item = Item.new
+    item.link_url = params.fetch("link_url")
+    item.link_description = params.fetch("link_description")
+    item.thumbnail_url = params.fetch("thumbnail_url")
+    item.save
+
+    redirect_to("/")
+
+    #render({:template => "item_templates/add"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-
   get("/", { :controller => "items", :action => "index" })
+  post("/new_item", { :controller => "items", :action => "add"})
+  get("/backdoor", { :controller => "items", :action => "form" })
 end
